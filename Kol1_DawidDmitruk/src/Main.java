b568ebfa06c19a73867ee5c0549b08fd817282a7
DawidDmitruk2 <dawid878ko@gmail.com>
2026-05-22 11:33:57 +0200

    Kol1 test

--- Kol1_DawidDmitruk/src/Main.java
+++ Kol1_DawidDmitruk/src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -73,5 +75,6 @@ public class Main {
             System.out.println("Zwyciezca wyborow: " + election.getWinner().name());
         }
     }
+
 
 }
